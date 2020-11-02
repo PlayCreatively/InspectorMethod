3476a130963b018396885b26544b495c88556f76
PlayCreatively <lexi-freyr@hotmail.com>
2020-11-02 02:13:49 +0000

    base

---
+++ MethodData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using TypeReferences;
+using UnityEngine;
+
+[Serializable]
+public class MethodData
+{
+    [SerializeField] protected string methodName;
+    [SerializeField] protected UnityEngine.Object targetInstance;
+    //[SerializeField] string delegateType;
+    [SerializeField] protected string[] parameters;
+    [SerializeField] protected int selectedIndex;
+    [SerializeField] protected string targetType;
+
+    public MethodInfo GetMethodInfo() => Type.GetType(targetType).GetMethod(methodName);
+
+    public T ToDelegate<T>() where T : Delegate
+    {
+        return Delegate.CreateDelegate(typeof(T), targetInstance, methodName) as T;
+    }
+
+    public Delegate ToDelegate()
+    {
+        Type type = GetMatchingDelegate(parameters.Length);
+        Debug.LogError(parameters.Length);
+
+        return Delegate.CreateDelegate(type, typeof(SerialTest), methodName);
+    }
+
+
+    public static Type[] StringsToTypes(string[] type)
+    {
+        return type.Select(name => Type.GetType(name)).ToArray();
+    }
+
+
+    public static Type GetMatchingDelegate(int parameterCount)
+    {
+        switch (parameterCount)
+        {
+            case 0: return typeof(Action);
+            case 1: return typeof(Action<dynamic>);
+            case 2: return typeof(Action<dynamic, dynamic>);
+            case 3: return typeof(Action<,,>);
+            case 4: return typeof(Action<,,,>);
+            default: return null;
+        }
+    }
+
+}
---
+++ MethodDataGeneric.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class MethodData<O> : MethodData
+{
+    public O Invoke()
+    {
+        object output = GetMethodInfo().Invoke(targetInstance, null);
+        if (output is O match) return match;
+        else return (O)output;
+    }
+}
+
+[Serializable]
+public class MethodData<O,I1> : MethodData
+{
+    public O Invoke(I1 value)
+    {
+        object output = GetMethodInfo().Invoke(targetInstance, new object[] { value }, true);
+        if (output is O match) return match;
+        else return (O)output;
+    }
+}
+
+[Serializable]
+public class MethodData<O, I1, I2> : MethodData
+{
+    public O Invoke(I1 value1, I2 value2)
+    {
+        object output = GetMethodInfo().Invoke(targetInstance, new object[] { value1, value2 }, true);
+        if (output is O match) return match;
+        else return (O)output;
+    }
+}
+
+[Serializable]
+public class MethodData<O, I1, I2, I3> : MethodData
+{
+    public O Invoke(I1 value1, I2 value2, I3 value3)
+    {
+        object output = GetMethodInfo().Invoke(targetInstance, new object[] { value1, value2, value3 }, true);
+        if (output is O match) return match;
+        else return (O) output;
+    }
+}
